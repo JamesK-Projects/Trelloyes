3f2d90ba0946bd6b7392ec3a4ef2bfa3965b94e9
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-09-10 11:26:54 -0600

    initial commit test files

---
+++ src/List.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+import renderer from 'react-test-renderer';
+
+//why do these tests fail?
+
+describe('List rendering', () => {
+    it('Renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<List cards={[]}/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<List name='List' cards={[]}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
